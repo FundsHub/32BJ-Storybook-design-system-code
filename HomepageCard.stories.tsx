576a4ee81a1b7d6968338f1a5c5eac93d5337871
FundsHub <emoore@32bjfunds.com>
2026-09-17 15:41:57 -0400

    Add storybook stories for HomepageCard component

--- HomepageCard.stories.tsx
+++ HomepageCard.stories.tsx
@@ -1,5 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { HomepageCard } from './HomepageCard';
+
+const meta: Meta<typeof HomepageCard> = {
+  title: 'Components/Cards',
+  component: HomepageCard,
+  tags: ['autodocs']
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HomepageCard>;
+
+export const MemberPortal: Story = {
+  args: {
+    type: 'member'
+  },
+  parameters: {
+    viewport: { defaultViewport: 'desktop1200' }
+  }
+};
+
+export const WhoWeAre: Story = {
+  args: {
+    type: 'who'
+  },
+  parameters: {
+    viewport: { defaultViewport: 'desktop1200' }
+  }
+};
+
+export const EqualHeightRow: Story = {
+  render: () => (
+    <div className="ds-card-row">
+      <HomepageCard type="member" />
+      <HomepageCard type="who" />
+    </div>
+  ),
+  parameters: {
+    viewport: { defaultViewport: 'desktop1200' }
+  }
+};
+
 export const MobileMemberPortal: Story = {
   args: {
+    type: 'member',
     mobile: true
   },
   parameters: {
