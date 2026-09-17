c31d5bf2c6cc33d8965c3daad0e1767e0b7fb465
FundsHub <emoore@32bjfunds.com>
2026-09-17 16:20:29 -0400

    Refactor MemberServices stories for viewport parameters

--- MemberServices.stories.tsx
+++ MemberServices.stories.tsx
@@ -1,17 +1,19 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { MemberServices } from './index';
-
-const meta: Meta<typeof MemberServices> = {
-  title: 'Components/Member Services',
-  component: MemberServices,
-  tags: ['autodocs']
+export const Desktop: Story = {
+  parameters: {
+    viewport: { defaultViewport: 'desktop1200' }
+  }
 };
-export default meta;
-type Story = StoryObj<typeof MemberServices>;
 
-export const Desktop: Story = {};
 export const Mobile: Story = {
   args: { mobile: true },
-  parameters: { viewport: { defaultViewport: 'mobile390' } }
+  parameters: {
+    viewport: { defaultViewport: 'mobile390' }
+  }
 };
-export const LegalTheme: Story = { args: { fund: 'legal' } };
+
+export const LegalTheme: Story = {
+  args: { fund: 'legal' },
+  parameters: {
+    viewport: { defaultViewport: 'desktop1200' }
+  }
+};
